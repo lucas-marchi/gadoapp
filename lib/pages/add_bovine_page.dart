15b750ce6bf9102d6d9c9b1c7ea399cf13fb6327
lucas-marchi <lucasmarchi20@gmail.com>
2024-11-12 15:18:36 -0300

    fields em adicionar bovino

--- lib/pages/add_bovine_page.dart
+++ lib/pages/add_bovine_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:gadoapp/customwidgets/radio_group.dart';
+import 'package:gadoapp/models/herds.dart';
+import 'package:gadoapp/providers/bovine_provider.dart';
+import 'package:gadoapp/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 class AddBovinePage extends StatefulWidget {
   static const String routeName = 'addbovine';
@@ -9,14 +14,55 @@ class AddBovinePage extends StatefulWidget {
 }
 
 class _AddBovinePageState extends State<AddBovinePage> {
+  Herd? herd;
+  final _formKey = GlobalKey<FormState>();
+  String bovineStatus = BovineUtils.statusList.first;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Adicionar bovino'),
       ),
-      body: Center(
-        child: Text('Add bovine'),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: const EdgeInsets.all(16),
+          children: [
+            Card(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Consumer<BovineProvider>(
+                      builder: (context, provider, child) =>
+                          DropdownButtonFormField<Herd>(
+                              decoration: const InputDecoration(
+                                  border: InputBorder.none),
+                              hint: const Text('Adicionar á um rebanho'),
+                              isExpanded: true,
+                              value: herd,
+                              items: provider.herdList
+                                  .map((item) => DropdownMenuItem<Herd>(
+                                      value: item, child: Text(item.name)))
+                                  .toList(),
+                              validator: (value) {
+                                if (value == null) {
+                                  return 'Selecione para qual rebanho será adicionado';
+                                }
+                                return null;
+                              },
+                              onChanged: (value) {
+                                herd = value;
+                              }),
+                    ))),
+            RadioGroup(
+              label: 'Status', 
+              groupValue: bovineStatus, 
+              items: BovineUtils.statusList, 
+              onItemSelected: (value) {
+                bovineStatus = value;
+              })
+          ],
+        ),
       ),
     );
   }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,8 @@
 abstract final class BovineUtils {
-  
+
+  static const String _alive = 'Vivo';
+  static const String _dead = 'Morto';
+  static const String _sold = 'Vendido';
+
+  static const statusList = [_alive, _dead, _sold];
 }
