42d3af22d548750b8d93e5b157e37abbe5c719e5
lucas-marchi <lucasmarchi20@gmail.com>
2025-02-01 02:02:49 -0300

    melhorias na visualização dos bovinos

--- lib/pages/view_bovine_page.dart
+++ lib/pages/view_bovine_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gadoapp/models/bovine.dart';
 import 'package:gadoapp/pages/add_bovine_page.dart';
 import 'package:gadoapp/pages/bovine_details_page.dart';
 import 'package:gadoapp/providers/bovine_provider.dart';
@@ -16,31 +17,59 @@ class ViewBovinePage extends StatefulWidget {
 class _ViewBovinePageState extends State<ViewBovinePage> {
   final _searchController = TextEditingController();
   String _searchText = '';
+  String? _selectedHerdId;
+  String? _selectedGender;
+  String? _selectedStatus;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: TextField(
-        controller: _searchController,
-        onChanged: (value) {
-          setState(() {
-            _searchText = value;
-          });
-        },
-        decoration: InputDecoration(
-          hintText: 'Pesquisar',
-          border: OutlineInputBorder(
-            borderSide:
-                BorderSide(color: Theme.of(context).colorScheme.primary),
-            borderRadius: BorderRadius.circular(16.0),
-          ),
+        title: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _searchController,
+                onChanged: (value) {
+                  setState(() {
+                    _searchText = value;
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: 'Pesquisar',
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary),
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: const Icon(Icons.filter_list),
+              onPressed: () {
+                _showFilterDialog(context);
+              },
+            ),
+          ],
         ),
-      )),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.goNamed(AddBovinePage.routeName);
+        },
+        child: const Icon(Icons.add),
+      ),
       body: Consumer<BovineProvider>(builder: (context, provider, child) {
         final filteredBovines = provider.bovineList
             .where((bovine) =>
-                bovine.name!.toLowerCase().contains(_searchText.toLowerCase()))
+                bovine.name!
+                    .toLowerCase()
+                    .contains(_searchText.toLowerCase()) &&
+                (_selectedHerdId == null ||
+                    bovine.herd?.id == _selectedHerdId) &&
+                (_selectedGender == null || bovine.gender == _selectedGender) &&
+                (_selectedStatus == null || bovine.status == _selectedStatus))
             .toList();
         return filteredBovines.isEmpty
             ? const Center(
@@ -69,7 +98,7 @@ class _ViewBovinePageState extends State<ViewBovinePage> {
                           child: Row(
                             children: [
                               Expanded(
-                                flex: 2, // Aumenta o espaço para o nome
+                                flex: 2,
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
@@ -146,5 +175,104 @@ class _ViewBovinePageState extends State<ViewBovinePage> {
         return const Icon(Icons.question_mark_rounded,
             color: Colors.grey, size: 18.0);
     }
+  }
+
+  void _showFilterDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Filtros'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Rebanho'),
+                value: _selectedHerdId,
+                items: _herdDropdownItems(context),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedHerdId = value;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Sexo'),
+                value: _selectedGender,
+                items: _sexDropdownItems(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedGender = value;
+                  });
+                },
+              ),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Status'),
+                value: _selectedStatus,
+                items: _statusDropdownItems(),
+                onChanged: (value) {
+                  setState(() {
+                    _selectedStatus = value;
+                  });
+                },
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              child: const Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: const Text('Aplicar'),
+              onPressed: () {
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  List<Bovine> _filterBovines(List<Bovine> bovines) {
+    return bovines
+        .where((bovine) =>
+            bovine.name!.toLowerCase().contains(_searchText.toLowerCase()) &&
+            (_selectedHerdId == null || bovine.herd?.id == _selectedHerdId) &&
+            (_selectedGender == null || bovine.gender == _selectedGender) &&
+            (_selectedStatus == null || bovine.status == _selectedStatus))
+        .toList();
+  }
+
+  List<DropdownMenuItem<String>> _herdDropdownItems(BuildContext context) {
+    final provider = Provider.of<BovineProvider>(context, listen: false);
+    return provider.herdList.map((herd) {
+      return DropdownMenuItem(
+        value: herd.id,
+        child: Text(herd.name),
+      );
+    }).toList();
+  }
+
+  List<DropdownMenuItem<String>> _sexDropdownItems() {
+    return ['Macho', 'Fêmea'].map((sex) {
+      return DropdownMenuItem(
+        value: sex,
+        child: Text(sex),
+      );
+    }).toList();
+  }
+
+  List<DropdownMenuItem<String>> _statusDropdownItems() {
+    return ['Vivo', 'Morto', 'Vendido'].map((status) {
+      return DropdownMenuItem(
+        value: status,
+        child: Text(status),
+      );
+    }).toList();
   }
 }
